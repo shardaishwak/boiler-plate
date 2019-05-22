de543778300ca17819a8cf3d10b12f6eae09cb56
Ishwak Sharda <ishwak.sharda@gmail.com>
2019-05-22 15:22:20 +0200

    added babel/proposal-class-properties for state

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
 import React from "react"
 import styles from "./App.module.scss"
 
-const App = props => {
-	return (
-		<div className={styles.container}>
-			<div className={styles.App_left}>
-				<h1>Ishwak Boilerplate</h1>
-				<p>This is my personal boilerplate for my react projects</p>
+class App extends React.PureComponent {
+	state = {
+		color: 2
+	}
+	render() {
+		console.log(this.state.color)
+
+		return (
+			<div className={styles.container}>
+				<div className={styles.App_left}>
+					<h1>Ishwak Boilerplate</h1>
+					<p>This is my personal boilerplate for my react projects</p>
+				</div>
+				<div className={styles.App_right}>
+					<div className={styles.command}>
+						<div className={styles.arrow}>></div>
+						git clone
+						https://www.github.com/ishwaksharda/ishwak-boiler-plate.git
+					</div>
+					<div className={styles.command}>
+						<div className={styles.arrow}>></div>
+						npm install{" "}
+					</div>
+					<div className={styles.command}>
+						<div className={styles.arrow}>></div>
+						npm start
+					</div>
+					<div className={styles.command}>
+						<div className={styles.arrow}>></div>
+						npm build
+					</div>
+					<div className={styles.command}>
+						<div className={styles.arrow}>></div>
+						npm test
+					</div>
+				</div>
 			</div>
-			<div className={styles.App_right}>
-				<div className={styles.command}>
-					<div className={styles.arrow}>></div>
-					git clone https://www.github.com/ishwaksharda/ishwak-boiler-plate.git
-				</div>
-				<div className={styles.command}>
-					<div className={styles.arrow}>></div>
-					npm install{" "}
-				</div>
-				<div className={styles.command}>
-					<div className={styles.arrow}>></div>
-					npm start
-				</div>
-				<div className={styles.command}>
-					<div className={styles.arrow}>></div>
-					npm build
-				</div>
-				<div className={styles.command}>
-					<div className={styles.arrow}>></div>
-					npm test
-				</div>
-			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default App
